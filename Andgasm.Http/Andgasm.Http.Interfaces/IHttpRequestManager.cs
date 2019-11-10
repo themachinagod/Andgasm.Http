b737f825d1127fc069024389ff7121e7f53cf66e
Danny Brown <machinagod@googlemail.com>
2019-11-10 14:59:30 +0000

    interface mods

---
+++ Andgasm.Http/Andgasm.Http.Interfaces/IHttpRequestManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Andgasm.Http.Interfaces
+{
+    public interface IHttpRequestManager
+    {
+        Task<HttpResponseMessage> Get(string requestUri);
+        Task<T> Get<T>(string apiroot, string apipath, params string[] pathvars);
+        Task<HttpResponseMessage> Post(string requestUri, object value);
+        Task Post<T>(T model, string apiroot, string apipath, params string[] pathvars);
+        Task<T> Post<T, K>(K model, string apiroot, string apipath, params string[] pathvars);
+        Task<HttpResponseMessage> Put(string requestUri, object value);
+        Task Put<T>(T model, string apiroot, string apipath, params string[] pathvars);
+        Task<T> Put<T, K>(K model, string apiroot, string apipath, params string[] pathvars);
+        Task<HttpResponseMessage> Patch(string requestUri, object value);
+        Task<HttpResponseMessage> Delete(string requestUri);
+        Task Delete<T>(string apiroot, string apipath, params string[] pathvars);
+
+    }
+}
--- Andgasm.Http/Andgasm.Http/HttpRequestManager.cs
+++ Andgasm.Http/Andgasm.Http/HttpRequestManager.cs
@@ -1,46 +1,47 @@
-﻿using System.Net.Http;
+﻿using Andgasm.Http.Interfaces;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Andgasm.Http
 {
-    public static class HttpRequestFactory
+    public class HttpRequestManager : IHttpRequestManager
     {
         #region Generic Model Serialisation
-        public static async Task<T> Get<T>(string apiroot, string apipath, params string[] pathvars)
+        public async Task<T> Get<T>(string apiroot, string apipath, params string[] pathvars)
         {
             var requestUri = BuildRequestUri(apiroot, apipath, pathvars);
             var response = await Get(requestUri.ToString());
             return response.ContentAsType<T>();
         }
 
-        public static async Task Post<T>(T model, string apiroot, string apipath, params string[] pathvars)
+        public async Task Post<T>(T model, string apiroot, string apipath, params string[] pathvars)
         {
             var requestUri = BuildRequestUri(apiroot, apipath, pathvars);
             await Post(requestUri.ToString(), model);
         }
 
-        public static async Task<T> Post<T, K>(K model, string apiroot, string apipath, params string[] pathvars)
+        public async Task<T> Post<T, K>(K model, string apiroot, string apipath, params string[] pathvars)
         {
             var requestUri = BuildRequestUri(apiroot, apipath, pathvars);
             var response = await Post(requestUri.ToString(), model);
             return response.ContentAsType<T>();
         }
 
-        public static async Task Put<T>(T model, string apiroot, string apipath, params string[] pathvars)
+        public async Task Put<T>(T model, string apiroot, string apipath, params string[] pathvars)
         {
             var requestUri = BuildRequestUri(apiroot, apipath, pathvars);
             await Put(requestUri.ToString(), model);
         }
 
-        public static async Task<T> Put<T, K>(K model, string apiroot, string apipath, params string[] pathvars)
+        public async Task<T> Put<T, K>(K model, string apiroot, string apipath, params string[] pathvars)
         {
             var requestUri = BuildRequestUri(apiroot, apipath, pathvars);
             var response = await Put(requestUri.ToString(), model);
             return response.ContentAsType<T>();
         }
 
-        public static async Task Delete<T>(string apiroot, string apipath, params string[] pathvars)
+        public async Task Delete<T>(string apiroot, string apipath, params string[] pathvars)
         {
             var requestUri = BuildRequestUri(apiroot, apipath, pathvars);
             await Delete(requestUri.ToString());
@@ -48,7 +49,7 @@ namespace Andgasm.Http
         #endregion
 
         #region Raw Response Serialisation
-        public static async Task<HttpResponseMessage> Get(string requestUri)
+        public async Task<HttpResponseMessage> Get(string requestUri)
         {
             var builder = new HttpRequestBuilder()
                                 .AddMethod(HttpMethod.Get)
@@ -57,7 +58,7 @@ namespace Andgasm.Http
             return await builder.SendAsync();
         }
 
-        public static async Task<HttpResponseMessage> Post(
+        public async Task<HttpResponseMessage> Post(
            string requestUri, object value)
         {
             var builder = new HttpRequestBuilder()
@@ -68,7 +69,7 @@ namespace Andgasm.Http
             return await builder.SendAsync();
         }
 
-        public static async Task<HttpResponseMessage> Put(
+        public async Task<HttpResponseMessage> Put(
            string requestUri, object value)
         {
             var builder = new HttpRequestBuilder()
@@ -79,7 +80,7 @@ namespace Andgasm.Http
             return await builder.SendAsync();
         }
 
-        public static async Task<HttpResponseMessage> Patch(
+        public async Task<HttpResponseMessage> Patch(
            string requestUri, object value)
         {
             var builder = new HttpRequestBuilder()
@@ -90,7 +91,7 @@ namespace Andgasm.Http
             return await builder.SendAsync();
         }
 
-        public static async Task<HttpResponseMessage> Delete(string requestUri)
+        public async Task<HttpResponseMessage> Delete(string requestUri)
         {
             var builder = new HttpRequestBuilder()
                                 .AddMethod(HttpMethod.Delete)
